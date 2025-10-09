78131b8bdc11bc5f681476b106192fd22d93d2da
riadh <riadh2002belg1cem@gmail.com>
2025-10-09 22:27:23 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
+// branch: main
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome to MAIN branch!");
+        System.out.println("Hello and welcome to MAIN");
 
         for (int i = 1; i <= 7; i++) {
             System.out.println("i = " + 2 * i);
         }
-
-        System.out.println("Execution finished on MAIN branch.");
     }
 }
